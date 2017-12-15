ac7933c4db10113d11bdd71c54aff6f489a39696
Ana Gheorghe <a.s.gheorghe3@gmail.com>
2017-12-14 16:11:31 -0800

    added rodentenv to gitignore and changed Hello.js

--- js/Hello.js
+++ js/Hello.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 var Hello = React.createClass({
   render() {
-    return <h1>Hello, world</h1>;
+    return (
+      <div>
+        <h1> abc TESTING 123  </h1>
+        <h3>Hello, world</h3>
+      </div>
+    );
   }
 });
 
 export default Hello;
-
