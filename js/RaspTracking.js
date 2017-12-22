101c39c92c58a3e59e1f6fbe0e4403140fbf104c
Ana Gheorghe <a.s.gheorghe3@gmail.com>
2017-12-21 17:17:10 -0800

    link styling

--- js/RaspTracking.js
+++ js/RaspTracking.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './styles/RaspTracking.css';
+import { Link } from 'react-router-dom'
+
 
 
 class RaspTracking extends React.Component {
