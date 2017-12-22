197ac3e71900fe70313e36944fe2425208492f42
Ana Gheorghe <a.s.gheorghe3@gmail.com>
2017-12-22 13:36:44 -0800

    addAnimal page styling no Pi

--- js/RaspAddAnimal.js
+++ js/RaspAddAnimal.js
@@ -87,28 +87,59 @@ class RaspAddAnimal extends React.Component {
 
 
   render() {
+        // return (
+        //   <div className="add-body">
+        //     <Link className="link-tag" to={`/raspExperiments/${this.props.match.params.id}/${this.props.match.params.cageId}`}> Back </Link>
+        //     <div className="add-title"> Add a New Animal </div>
+        //     <button className="add-button" type="button" onClick={() => this.scanAnimal()}> Scan animal </button>
+        //     <label className="add-label"> RFID: <input className="add-inputs" type="text" name="rfid" value={this.state.rfid}/> </label>
+        //
+        //     {this.state.scanned ? (<form className="add-animal-form" onSubmit={(event) => this.handleSubmit(event)}>
+        //     <label className="add-label"> <p>ExperimentID: </p>  <input className="add-inputs" type="text" name="experimentID" value={this.props.match.params.id}/> </label><br/>
+        //     <label className="add-label"> <p> Cage: </p>  <input className="add-inputs" type="text" name="cage" defaultValue={this.props.match.params.cageId}/> </label> <br/>
+        //     <label className="add-label"> Age: <input className="add-inputs" type="text" name="age" value={this.state.age} onChange={this.handleChange.bind(this)}/> </label> <br/>
+        //     <label className="add-label"> Sex: <input className="add-inputs" type="text" name="sex" value={this.state.sex} onChange={this.handleChange.bind(this)}/> </label> <br/>
+        //     <label className="add-label"> Notes: <input className="add-inputs" type="text" name="notes" value={this.state.notes} onChange={this.handleChange.bind(this)}/> </label> <br/>
+        //     <input className="add-label" type="submit" value="Add Animal" />
+        //     <Link to="/raspTracking">Track Animals</Link>
+        //
+        //     </form> ) : ''}
+        //     <div className="message-text">
+        //       {this.state.message ? <p> {this.state.message} </p> : '' }
+        //     </div>
+        //   </div>
+        // );
         return (
-          <div className="add-body">
-            <Link className="link-tag" to={`/raspExperiments/${this.props.match.params.id}/${this.props.match.params.cageId}`}> Back </Link>
-            <div className="add-title"> Add a New Animal </div>
-            <button className="add-button" type="button" onClick={() => this.scanAnimal()}> Scan animal </button>
-            <label className="add-label"> RFID: </label> <input className="add-inputs" type="text" name="rfid" value={this.state.rfid} />
+          <div className="page-box">
+            <h1>Add Animal</h1>
+            <div className="form-box">
+              <div className="form-style-3">
+                <form>
+                  <fieldset><legend>New Animal Information</legend>
+                  <label htmlFor="field1"><span>RFID <span className="required">*</span></span><input type="text" className="input-field" name="field1" value="" /></label>
+                  <label htmlFor="field2"><span>Experiment ID <span className="required">*</span></span><input type="email" className="input-field" name="field2" value="" /></label>
+                  <label htmlFor="field3"><span>Cage ID <span className="required">*</span></span><input type="text" className="input-field" name="field3" value="" /></label>
+                  <label htmlFor="field4"><span>Sex <span className="required">*</span></span>
+                  <input type="text" name="field4" value="" />
 
-            {this.state.scanned ? (<form onSubmit={(event) => this.handleSubmit(event)}>
-            <label className="add-label"> ExperimentID: </label> <input className="add-inputs" type="text" name="experimentID" value={this.props.match.params.id}/> <br/>
-            <label className="add-label"> Cage: </label> <input className="add-inputs" type="text" name="cage" defaultValue={this.props.match.params.cageId}/> <br/>
-            <label className="add-label"> Age: </label> <input className="add-inputs" type="text" name="age" value={this.state.age} onChange={this.handleChange.bind(this)} /> <br/>
-            <label className="add-label"> Sex: </label> <input className="add-inputs" type="text" name="sex" value={this.state.sex} onChange={this.handleChange.bind(this)} /> <br/>
-            <label className="add-label"> Notes: </label> <input className="add-inputs" type="text" name="notes" value={this.state.notes} onChange={this.handleChange.bind(this)} /> <br/>
-            <input className="add-label" type="submit" value="Add Animal" />
-            <Link to="/raspTracking">Track Animals</Link>
+                  {/* <select name="field4" className="input-field">
+                    <option value="Appointment">Female</option>
+                    <option value="Interview">Male</option>
+                  </select> */}
+                </label>
+                  <label htmlFor="field5"><span>Age <span className="required">*</span></span><input type="text" className="input-field" name="field5" value="" /></label>
+                  <label for="field6"><span>Notes </span><textarea name="field6" class="textarea-field"></textarea></label>
 
-            </form> ) : ''}
-            <div className="message-text">
-              {this.state.message ? <p> {this.state.message} </p> : '' }
+                  <input style={{ backgroundColor: '#0074D9' }} type="submit" value="Add Animal" />
+
+                </fieldset>
+
+
+              </form>
             </div>
           </div>
-        );
+        </div>
+        )
   }
 };
 
